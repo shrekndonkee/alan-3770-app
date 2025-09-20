43c781f2de0835e7b1c5978674da83fc097af330
Alan Martinez-Diaz <alanmartindiaz21@gmail.com>
2025-09-20 16:26:04 -0600

    added introduction and some tutorial stuff.

---
+++ src/lib/components/BigIntro.svelte
@@ -0,0 +1,3 @@
+<div class="vh-75 flex flex-col justify-center items-center text-center px-4">
+  <h1 class="text-4xl md:text-5xl font-bold mb-4">Welcome to Our Application!</h1>
+</div>
---
+++ src/routes/tutorials/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import BigIntro from "$lib/components/BigIntro.svelte";
+  let src= "dog.jpg"
+  let contentMessage = `This is the tutorial <strong>page content</strong> It can include HTML tags.`
+</script>
+<h1 class="text-2xl">Welcome to the tutorial page!</h1>
+<BigIntro/>
+<p>{@html contentMessage}</p>
+<img {src} alt="dog" class="mx-auto mt-4" />
