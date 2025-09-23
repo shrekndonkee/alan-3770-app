b5e455cbf1eec5c703aa12728f677d15f462bace
Alan Martinez-Diaz <alanmartindiaz21@gmail.com>
2025-09-22 21:47:42 -0600

    added logic and examples.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<nav>
+	<ul class="flex gap-4 p-2 bg-amber-400">
+		<li><a href="/">Home</a></li>
+		<li><a href="/tutorial">Tutorial Home</a></li>
+		<li><a href="/tutorial/reactivity">Reactivity</a></li>
+		<li><a href="/tutorial/props">Props</a></li>
+		<li><a href="/tutorial/logic">Logic</a></li>
+	</ul>
+</nav>
 
 {@render children?.()}
